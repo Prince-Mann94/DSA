1bbd929bd4a01c6117a25432f452d7fd70e75d98
Prince-Mann94 <princemann2007@gmail.com>
2026-01-25 23:01:44 +0530

    Delete Basic C/herons.py

--- Basic C/herons.py
+++
@@ -1,9 +0,0 @@
-import math
-a = int(input("Enter first num : "))
-b = int(input("Enter second num : "))
-c = int(input("Enter third num : "))
-
-s = (a+b+c)/2
-area = s*(s-a)*(s-b)*(s-c)
-
-print("Area of triangle using heron's formula : %.2f"% math.sqrt(area))
