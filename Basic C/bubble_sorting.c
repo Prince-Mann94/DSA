e0de6c1ac3ffbe201e5195ba4972f1fe7e7a873b
Prince-Mann94 <princemann2007@gmail.com>
2026-01-28 18:49:05 +0530

    Enhance bubble sort with comparison count
    Added a comparison count feature to the bubble sort algorithm.

--- Basic C/bubble_sorting.c
+++ Basic C/bubble_sorting.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 int main(){
     int a[5];
+    int count = 0;
     // bubble sorting
     printf("Enter elements of array : ");
     for(int i=0;i<5;i++){
@@ -12,12 +13,13 @@ int main(){
         printf("%d ",a[i]);
     }
 
-    for(int i=0;i<5;i++){
-        for(int j=i+1;j<5;j++){
-            if(a[i] > a[j]){
-                int temp = a[i];
-                a[i] = a[j];
-                a[j] = temp;
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4-i;j++){
+            count++;
+            if(a[j] > a[j+1]){
+                int temp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = temp;
             }
         }
     }
@@ -25,5 +27,7 @@ int main(){
         for(int i=0;i<5;i++){
             printf("%d ",a[i]);
         }
+
+    printf("Comparison count : %d",count);   // formula = (n(n-1)/2)
 
 }
